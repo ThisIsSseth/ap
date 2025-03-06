930372530b504d8caae0e6b417e326a0bb32ad56
ThisIsSseth <setayesh.ziyaee@gmail.com>
2025-03-06 20:43:03 +0330

    E6_1

--- bigJavaExercises/src/E6_1_a_e.java
+++ bigJavaExercises/src/E6_1_a_e.java
@@ -4,22 +4,22 @@ public class E6_1_a_e {
     public static void main(String[] args) {
         //a
         int sum = 2;
-        for (int i = 4; i < 202; i += 2)
+        for (int i = 4; i < 102; i += 2)
             sum += i;
         System.out.println("Part a: " + sum);
         //e
         System.out.println("Enter a  number: ");
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int oddSum = 0, temp;
+        int oddSum = 0, remaining;
         while (a != 0){
-            temp = a % 10;
-            if (temp % 2 != 0)
+            remaining = a % 10;
+            if (remaining % 2 != 0)
             {
-                oddSum += temp;
+                oddSum += remaining;
             }
-            a /=
+            a /= 10;
         }
-
+        System.out.println("Part e: " + oddSum);
     }
 }
