75e1dec8d3ba79be33d81f0fc4ac9966c3a28479
ThisIsSseth <setayesh.ziyaee@gmail.com>
2025-03-07 23:57:44 +0330

    E6_18

--- bigJavaExercises/src/E6_18.java
+++ bigJavaExercises/src/E6_18.java
@@ -6,5 +6,22 @@ public class E6_18 {
         System.out.print("Enter an integer: ");
         int num = sc.nextInt();
 
+        for (int i = 1 ; i <= num ; i++){
+            for (int j = num - i ; j > 0 ; j--)
+                System.out.print(" ");
+            for (int j = i*2 - 1 ; j > 0 ; j-- )
+                System.out.print("*");
+            System.out.println();
+        }
+        for (int i = num - 1 ; i >= 1 ; i--) {
+            for (int j = 1 ; j <= num - i ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1 ; j <= 2 * i - 1 ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
     }
 }
