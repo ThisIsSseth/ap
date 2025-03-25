4a831b7d2f3f66f85e1c7a6b57ecbe00af658ab6
ThisIsSseth <setayesh.ziyaee@gmail.com>
2025-03-25 13:45:28 +0330

    EX2_PM_1_2

--- bigJavaExercises/src/ap/exercises/ex2/Main_EX2_PM_1_2.java
+++ bigJavaExercises/src/ap/exercises/ex2/Main_EX2_PM_1_2.java
@@ -2,33 +2,30 @@ package ap.exercises.ex2;
 
 import java.util.Scanner;
 
-public class Main_EX2_PM_1_1 {
+public class Main_EX2_PM_1_2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter a number (1-127):\n");
         byte k = sc.nextByte();
-        if (k < 0 ) {
+        if (k < 0) {
             System.out.println("Invalid number");
-        }
-        else {
-            byte[][] board = new byte[k+2][k+2];
-            for (int i = 0; i < k+2; i++) {
-                for (int j = 0 ; j < k+2; j++) {
-                    if (i == 0 || j == 0 || i == k+1 || j == k+1) {
+        } else {
+            byte[][] board = new byte[k + 2][k + 2];
+            for (int i = 0; i < k + 2; i++) {
+                for (int j = 0; j < k + 2; j++) {
+                    if (i == 0 || j == 0 || i == k + 1 || j == k + 1) {
                         board[i][j] = '1';
-                    }
-                    else {
+                    } else {
                         board[i][j] = '0';
                     }
                 }
             }
             //now we print:
-            for (int i = 0; i < k+2; i++) {
-                for (int j = 0 ; j < k+2; j++) {
+            for (int i = 0; i < k + 2; i++) {
+                for (int j = 0; j < k + 2; j++) {
                     if (board[i][j] == '1') {
                         System.out.print("*");
-                    }
-                    else {
+                    } else {
                         System.out.print(" ");
                     }
                 }
