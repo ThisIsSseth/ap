bbc2e2ad43d66b2878bfd99db68320b093d6ec6d
ThisIsSseth <setayesh.ziyaee@gmail.com>
2025-04-04 00:20:56 +0330

    EX2_PM_2_3

--- bigJavaExercises/src/ap/exercises/ex2/Main_EX2_PM_2_2.java
+++ bigJavaExercises/src/ap/exercises/ex2/Main_EX2_PM_2_2.java
@@ -1,38 +1,68 @@
 package ap.exercises.ex2;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main_EX2_PM_2_2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the dimension (1-127):");
-        byte k = sc.nextByte();
-        if (k < 0) {
-            System.out.println("Invalid number");
+        Board board = new Board();
+        byte c = 0;
+        System.out.println("Checking if there are any saved games...");
+        if (board.memoryChecker()) {
+            board.boardReader();
+            c = board.getK();
+            System.out.println("Loading Saved game...");
         } else {
-            Board board = new Board();
-            board.createBoard(k);
-            System.out.println("enter a  of dots (1-127):");
-            byte c = sc.nextByte();
-            while (c >= k * k || c < 0) {
-                System.out.println("out of range! try again:");
+            System.out.println("No saved memory.\nStarting a new game...");
+            System.out.println("enter the dimension (2-127):");
+            byte k = sc.nextByte();
+            if (k < 1) {
+                System.out.println("Invalid number");
+            } else {
+                board.createBoard(k);
+                System.out.println("enter a  of dots (1-127):");
                 c = sc.nextByte();
+                while (c >= k * k || c < 0) {
+                    System.out.println("out of range! try again:");
+                    c = sc.nextByte();
+                }
+                board.dotter(c);
             }
-            board.dotter(c);
-            board.print();
-            byte move;
-            long startTime = System.currentTimeMillis();
-            while (board.getNumOfDot() > 0) {
-                System.out.println("enter a move:\n0.up\n1.right\n2.down\n3.left");
-                move = sc.nextByte();
+        }
+        board.print();
+        byte move = 0;
+        long startTime = System.currentTimeMillis();
+        
+        while (board.getNumOfDot() > 0 && move != 4) {
+
+            System.out.println("enter a move:\n0.up\n1.right\n2.down\n3.left\n4. Save and Exit");
+            move = sc.nextByte();
+            if (move > -1 && move < 4) {
                 board.xMove(move);
                 board.print();
+            } else if (move == 4) {
+                board.boardSaver(System.currentTimeMillis() - startTime);
+                System.out.println("Saved!");
+                System.exit(0);
+            } else {
+                System.out.println("invalid move");
             }
-            long endTime = System.currentTimeMillis();
-            System.out.println(endTime - startTime);
-            System.out.println("You scored " + board.getNumOfDot() + " dots!");
+
         }
+        long endTime = System.currentTimeMillis();
+        if (c == board.numOfDot) {
+            board.memoryDelete();
+        }
+        System.out.println(endTime - startTime);
+        System.out.println("You scored " + board.getNumOfDot() + " dots!");
+
         sc.close();
     }
 
@@ -42,8 +72,10 @@ public class Main_EX2_PM_2_2 {
         private byte K;
         private byte[][] board;
         private byte xPosX = 1, xPosY = 1;
-        public byte numOfDot;
+        private byte numOfDot;
         //wall = 1, pacman(X) = 2, dot = -1
+        File gameState;
+        long playingTime = 0;
 
         public void createBoard(byte k) {
             K = k;
@@ -68,14 +100,14 @@ public class Main_EX2_PM_2_2 {
             while (c > 0) {
                 intRandRow = rand.nextInt(K) + 1;
                 intRandCol = rand.nextInt(K) + 1;
-                if (board[intRandRow][intRandCol] != -1 || !(intRandRow == 1 && intRandCol == 1 )) {
+                if (board[intRandRow][intRandCol] != -1 || !(intRandRow == 1 && intRandCol == 1)) {
                     board[intRandRow][intRandCol] = -1;
                     c--;
                 }
             }
         }
 
-        public void print() {
+        public void print() throws IOException {
             for (int i = 0; i < K + 2; i++) {
                 for (int j = 0; j < K + 2; j++) {
                     if (board[i][j] == 1) {
@@ -92,7 +124,7 @@ public class Main_EX2_PM_2_2 {
             }
         }
 
-        public void xMove(byte move) {
+        public void xMove(byte move) throws IOException {
             switch (move) {
                 case 0:
                     move = 'w';
@@ -101,12 +133,10 @@ public class Main_EX2_PM_2_2 {
                     move = 's';
                     break;
                 case 3:
-                    move = 'a';
-                    break;
-                case 1:
                     move = 'd';
                     break;
-                default:
+                case 1:
+                    move = 'a';
                     break;
             }
             switch (move) {
@@ -150,18 +180,83 @@ public class Main_EX2_PM_2_2 {
                     } else
                         System.out.println("hitting a wall!");
                     break;
-                default:
-                    System.out.println("invalid move");
-                    break;
             }
         }
-        private void dotChecker(){
+
+        private void dotChecker() {
             if (board[xPosY][xPosX] == -1) {
                 numOfDot--;
             }
         }
-        public byte getNumOfDot(){
+
+        public byte getNumOfDot() {
             return numOfDot;
+        }
+
+        public byte getK() {
+            return K;
+        }
+
+
+        private void boardSaver(long time) throws IOException {
+            playingTime += time;
+            try (FileWriter writer = new FileWriter("GameState.txt");) {
+                writer.write(K + "," + numOfDot + "," + xPosX + "," + xPosY + ',' + playingTime + "\n");
+                for (byte[] row : board) {
+                    for (byte c : row) {
+                        writer.write(c + ",");
+                    }
+                    writer.write("\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        public void boardReader() {
+            String line;
+            String[] eachLine;
+            try (FileReader fr = new FileReader("GameState.txt"); Scanner scanner = new Scanner(fr);) {
+                line = scanner.nextLine();
+                eachLine = line.split(",");
+                K = Byte.parseByte(eachLine[0]);
+                board = new byte[K + 2][K + 2];
+                numOfDot = Byte.parseByte(eachLine[1]);
+                xPosX = Byte.parseByte(eachLine[2]);
+                xPosY = Byte.parseByte(eachLine[3]);
+                try {
+                    playingTime = Long.parseLong(eachLine[4]);
+                } catch (NumberFormatException e) {
+                }
+                createBoard(K);
+                for (int i = 0; i < K + 2; i++) {
+                    if (scanner.hasNextLine()) {
+                        line = scanner.nextLine();
+                        eachLine = line.split(",");
+                        for (int j = 0; j < K + 2; j++) {
+                            board[i][j] = Byte.parseByte(eachLine[j]);
+                        }
+
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        public void memoryDelete() {
+            try {
+                Files.delete(Paths.get("GameState.txt"));  // Using NIO (Java 7+)
+                System.out.println("File deleted successfully!");
+            } catch (IOException e) {
+                System.err.println("Failed to delete file: " + e.getMessage());
+            }
+        }
+
+        public boolean memoryChecker() throws IOException {
+            return Files.exists(Paths.get("GameState.txt")) && Files.size(Paths.get("GameState.txt")) > 0;
         }
     }
 }
