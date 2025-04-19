a2a9ead8bbf99eb0f21767f5d7801ceb1c82c939
ThisIsSseth <setayesh.ziyaee@gmail.com>
2025-04-19 22:37:12 +0330

    additional classes

---
+++ bigJavaExercises/src/ap/exercises/ex3/Book.java
@@ -0,0 +1,60 @@
+package ap.exercises.ex3;
+
+class Book {
+
+    private String title;
+    private String author;
+    private int pages;
+    private int borrower = 0;
+
+    private int publicationYear;
+
+    public int getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(int borrower) {
+        this.borrower = borrower;
+    }
+
+    public Book(String title, String author, int pages, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.publicationYear = publicationYear;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+}
---
+++ bigJavaExercises/src/ap/exercises/ex3/Student.java
@@ -0,0 +1,51 @@
+package ap.exercises.ex3;
+
+import static ap.exercises.ex3.Main_EX3_LM_1_3.studentId;
+
+public  class Student {
+
+    private String firstName;
+    private String lastName;
+    private int id;
+    private String major;
+
+    public Student(String firstName, String lastName, int id, String major) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        studentId.add(id);
+        this.major = major;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+
+}
+
