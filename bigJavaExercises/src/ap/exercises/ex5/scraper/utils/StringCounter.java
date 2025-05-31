26e48d99d538cab33019d2d7dd5f1a76f41a30e8
ThisIsSseth <setayesh.ziyaee@gmail.com>
2025-06-01 01:11:51 +0330

    EX5

---
+++ bigJavaExercises/src/ap/exercises/ex5/scraper/utils/StringCounter.java
@@ -0,0 +1,53 @@
+package ap.exercises.ex5.scraper.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class StringCounter {
+
+    private List<StringCountHolder> stringCountHolder = new ArrayList<>();
+
+    public void add(String string) {
+        for (StringCountHolder s : stringCountHolder) {
+            if (Objects.equals(string, s.getString())) {
+                s.increaseCount();
+                return;
+            }
+        }
+        stringCountHolder.add(new StringCountHolder(string));
+    }
+
+    public List<StringCountHolder> getTop(int k) {
+        return stringCountHolder.stream()
+                .sorted((a, b) -> Long.compare(a.getCount(), b.getCount()))
+                .limit(k).toList();
+
+    }
+}
+
+/**
+ * a lil class object to hold count of the strings
+ */
+class StringCountHolder {
+    private final String string;
+    private long count;
+
+    public StringCountHolder(String string) {
+        this.string = string;
+        this.count = 1;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public long getCount() {
+        return count;
+    }
+
+    public void increaseCount() {
+        count++;
+    }
+
+}
